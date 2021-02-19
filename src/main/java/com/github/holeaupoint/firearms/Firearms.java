df0c4aa9a11df69bd044cb69aa06961faf68e909
radiolite <47871545+radiolite@users.noreply.github.com>
2021-02-19 03:53:47 -0800

    Initial commit

---
+++ src/main/java/com/github/holeaupoint/firearms/Firearms.java
@@ -0,0 +1,10 @@
+package com.github.holeaupoint.firearms;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Firearms implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
